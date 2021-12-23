e75d51580f09281dbd63629d67a10cbf1d0bde1c
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2021-12-23 15:51:56 +0900

    debug

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,7 +41,9 @@ const config: PlaywrightTestConfig = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     ignoreHTTPSErrors: true
   },
 
@@ -100,7 +102,7 @@ const config: PlaywrightTestConfig = {
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+  outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
   // webServer: {

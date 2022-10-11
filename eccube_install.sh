036e2b83f31acc2da66db952aa02ae5ea950855e
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2022-10-11 14:37:46 +0900

    Add Dockerfile

--- eccube_install.sh
+++ eccube_install.sh
@@ -25,42 +25,59 @@
 #######################################################################
 
 #######################################################################
-# Configuration 
+# Configuration
 #-- Shop Configuration
 CONFIG_PHP="data/config/config.php"
 ADMIN_MAIL=${ADMIN_MAIL:-"admin@example.com"}
 SHOP_NAME=${SHOP_NAME:-"EC-CUBE SHOP"}
-HTTP_URL=${HTTP_URL:-"http://test.local"}
-HTTPS_URL=${HTTPS_URL:-"http://test.local/"}
+HTTP_URL=${HTTP_URL:-"http://localhost:8000/"}
+HTTPS_URL=${HTTPS_URL:-"http://localhost:8000/"}
+ROOT_URLPATH=${ROOT_URLPATH:-"/"}
 DOMAIN_NAME=${DOMAIN_NAME:-""}
+ADMIN_DIR=${ADMIN_DIR:-"admin/"}
+
+DBSERVER=${DBSERVER-"127.0.0.1"}
+DBNAME=${DBNAME:-"eccube_db"}
+DBUSER=${DBUSER:-"eccube_db_user"}
+DBPASS=${DBPASS:-"password"}
+
+MAIL_BACKEND=${MAIL_BACKEND-"smtp"}
+SMTP_HOST=${SMTP_HOST-"127.0.0.1"}
+SMTP_PORT=${SMTP_PORT-"1025"}
+SMTP_USER=${SMTP_USER-""}
+SMTP_PASSWORD=${SMTP_PASSWORD-""}
+
 ADMINPASS="f6b126507a5d00dbdbb0f326fe855ddf84facd57c5603ffdf7e08fbb46bd633c"
 AUTH_MAGIC="droucliuijeanamiundpnoufrouphudrastiokec"
 
 DBTYPE=$1;
 
 case "${DBTYPE}" in
-"pgsql" ) 
-    #-- DB Seting Postgres
+"heroku" )
     PSQL=psql
-    PGUSER=postgres
-    DROPDB=dropdb
-    CREATEDB=createdb
-    DBSERVER="127.0.0.1"
-    DBNAME=cube212_dev
-    DBUSER=cube212_dev_user
-    DBPASS=password
-    DBPORT=5432
+    export PGPASSWORD=${PGPASSWORD-$DBPASS}
+    PGUSER=${PGUSER-"postgres"}
+    DBPORT=${DBPORT:-"5432"}
 ;;
-"mysql" ) 
-    #-- DB Seting MySQL
+"appveyor" )
+    PSQL=psql
+    export PGPASSWORD=${PGPASSWORD-$DBPASS}
+    PGUSER=postgres
+    DBPORT=${DBPORT:-"5432"}
+;;
+"pgsql" )
+    PSQL=psql
+    export PGPASSWORD=${PGPASSWORD-$DBPASS}
+    PGUSER=postgres
+    DBPORT=${DBPORT:-"5432"}
+    DB=$DBTYPE;
+;;
+"mysql" | "mysqli" )
     MYSQL=mysql
     ROOTUSER=root
-    ROOTPASS=password
-    DBSERVER="127.0.0.1"
-    DBNAME=cube212_dev
-    DBUSER=cube212_dev_user
-    DBPASS=password
-    DBPORT=3306
+    ROOTPASS=${ROOTPASS-$DBPASS}
+    DBPORT=${DBPORT:-"3306"}
+    DB=mysql;
 ;;
 * ) echo "ERROR:: argument is invaid"
 exit
@@ -69,102 +86,204 @@ esac
 
 
 #######################################################################
-# Install 
+# Functions
+
+adjust_directory_permissions()
+{
+    chmod -R go+w "./html"
+    chmod go+w "./data"
+    chmod -R go+w "./data/Smarty"
+    chmod -R go+w "./data/cache"
+    chmod -R go+w "./data/class"
+    chmod -R go+w "./data/class_extends"
+    chmod go+w "./data/config"
+    chmod -R go+w "./data/download"
+    chmod -R go+w "./data/downloads"
+    chmod go+w "./data/fonts"
+    chmod go+w "./data/include"
+    chmod go+w "./data/logs"
+    chmod -R go+w "./data/module"
+    chmod go+w "./data/smarty_extends"
+    chmod go+w "./data/upload"
+    chmod go+w "./data/upload/csv"
+}
+
+create_sequence_tables()
+{
+    SEQUENCES="
+dtb_best_products_best_id_seq
+dtb_bloc_bloc_id_seq
+dtb_category_category_id_seq
+dtb_class_class_id_seq
+dtb_classcategory_classcategory_id_seq
+dtb_csv_no_seq
+dtb_csv_sql_sql_id_seq
+dtb_customer_customer_id_seq
+dtb_deliv_deliv_id_seq
+dtb_holiday_holiday_id_seq
+dtb_kiyaku_kiyaku_id_seq
+dtb_mail_history_send_id_seq
+dtb_maker_maker_id_seq
+dtb_member_member_id_seq
+dtb_module_update_logs_log_id_seq
+dtb_news_news_id_seq
+dtb_order_order_id_seq
+dtb_order_detail_order_detail_id_seq
+dtb_other_deliv_other_deliv_id_seq
+dtb_pagelayout_page_id_seq
+dtb_payment_payment_id_seq
+dtb_products_class_product_class_id_seq
+dtb_products_product_id_seq
+dtb_review_review_id_seq
+dtb_send_history_send_id_seq
+dtb_mailmaga_template_template_id_seq
+dtb_plugin_plugin_id_seq
+dtb_plugin_hookpoint_plugin_hookpoint_id_seq
+dtb_api_config_api_config_id_seq
+dtb_api_account_api_account_id_seq
+dtb_tax_rule_tax_rule_id_seq
+"
+
+    comb_sql="";
+    for S in $SEQUENCES; do
+        case ${DBTYPE} in
+            heroku | appveyor | pgsql )
+                sql=$(echo "CREATE SEQUENCE ${S} START 10000;")
+            ;;
+            mysql | mysqli )
+                sql=$(echo "CREATE TABLE ${S} (
+                        sequence int(11) NOT NULL AUTO_INCREMENT,
+                        PRIMARY KEY (sequence)
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+                    LOCK TABLES ${S} WRITE;
+                    INSERT INTO ${S} VALUES (10000);
+                    UNLOCK TABLES;")
+            ;;
+        esac
+
+        comb_sql=${comb_sql}${sql}
+    done;
+
+    case ${DBTYPE} in
+        heroku | appveyor | pgsql )
+            echo ${comb_sql} | ${PSQL} -h ${DBSERVER} -p ${DBPORT} -U ${DBUSER} ${DBNAME}
+        ;;
+        mysql)
+            echo ${comb_sql} | ${MYSQL} -h ${DBSERVER} -P ${DBPORT} -u ${DBUSER} ${PASSOPT} ${DBNAME}
+        ;;
+    esac
+}
+
+get_optional_sql()
+{
+    echo "INSERT INTO dtb_member (member_id, login_id, password, name, salt, work, del_flg, authority, creator_id, rank, update_date) VALUES (2, 'admin', '${ADMINPASS}', '管理者', '${AUTH_MAGIC}', '1', '0', '0', '0', '1', current_timestamp);"
+    echo "INSERT INTO dtb_baseinfo (id, shop_name, email01, email02, email03, email04, top_tpl, product_tpl, detail_tpl, mypage_tpl, update_date) VALUES (1, '${SHOP_NAME}', '${ADMIN_MAIL}', '${ADMIN_MAIL}', '${ADMIN_MAIL}', '${ADMIN_MAIL}', 'default1', 'default1', 'default1', 'default1', current_timestamp);"
+}
+
+create_config_php()
+{
+    cat > "./${CONFIG_PHP}" <<__EOF__
+<?php
+defined('ECCUBE_INSTALL') or define('ECCUBE_INSTALL', 'ON');
+defined('HTTP_URL') or define('HTTP_URL', '${HTTP_URL}');
+defined('HTTPS_URL') or define('HTTPS_URL', '${HTTPS_URL}');
+defined('ROOT_URLPATH') or define('ROOT_URLPATH', '${ROOT_URLPATH}');
+defined('DOMAIN_NAME') or define('DOMAIN_NAME', '${DOMAIN_NAME}');
+defined('DB_TYPE') or define('DB_TYPE', '${DB}');
+defined('DB_USER') or define('DB_USER', '${DBUSER}');
+defined('DB_PASSWORD') or define('DB_PASSWORD', '${CONFIGPASS:-$DBPASS}');
+defined('DB_SERVER') or define('DB_SERVER', '${DBSERVER}');
+defined('DB_NAME') or define('DB_NAME', '${DBNAME}');
+defined('DB_PORT') or define('DB_PORT', '${DBPORT}');
+defined('ADMIN_DIR') or define('ADMIN_DIR', '${ADMIN_DIR}');
+defined('ADMIN_FORCE_SSL') or define('ADMIN_FORCE_SSL', FALSE);
+defined('ADMIN_ALLOW_HOSTS') or define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
+defined('AUTH_MAGIC') or define('AUTH_MAGIC', '${AUTH_MAGIC}');
+defined('PASSWORD_HASH_ALGOS') or define('PASSWORD_HASH_ALGOS', 'sha256');
+defined('MAIL_BACKEND') or define('MAIL_BACKEND', '${MAIL_BACKEND}');
+defined('SMTP_HOST') or define('SMTP_HOST', '${SMTP_HOST}');
+defined('SMTP_PORT') or define('SMTP_PORT', '${SMTP_PORT}');
+defined('SMTP_USER') or define('SMTP_USER', '${SMTP_USER}');
+defined('SMTP_PASSWORD') or define('SMTP_PASSWORD', '${SMTP_PASSWORD}');
+
+__EOF__
+
+    cat "./${CONFIG_PHP}"
+}
+
+
+#######################################################################
+# Install
 
 #-- Update Permissions
 echo "update permissions..."
-chmod -R 777 "./html"
-chmod 755 "./data"
-chmod -R 777 "./data/Smarty"
-chmod -R 777 "./data/cache"
-chmod -R 777 "./data/class"
-chmod -R 755 "./data/class_extends"
-chmod 777 "./data/config"
-chmod -R 777 "./data/download"
-chmod -R 777 "./data/downloads"
-chmod 755 "./data/fonts"
-chmod 755 "./data/include"
-chmod 777 "./data/logs"
-chmod -R 777 "./data/module"
-chmod 755 "./data/smarty_extends"
-chmod 777 "./data/upload"
-chmod 777 "./data/upload/csv"
+adjust_directory_permissions
 
 #-- Setup Database
 SQL_DIR="./html/install/sql"
-OPTIONAL_SQL_FILE=optional.sql
-if [ -f ${OPTIONAL_SQL_FILE} ]
-then
-    echo "remove optional SQL"
-    rm ${OPTIONAL_SQL_FILE}
-fi
-
-echo "create optional SQL..."
-echo "INSERT INTO dtb_member (member_id, login_id, password, salt, work, del_flg, authority, creator_id, rank, update_date) VALUES (2, 'admin', '${ADMINPASS}', '${AUTH_MAGIC}', '1', '0', '0', '0', '1', current_timestamp);" >> ${OPTIONAL_SQL_FILE}
-echo "INSERT INTO dtb_baseinfo (id, shop_name, email01, email02, email03, email04, email05, top_tpl, product_tpl, detail_tpl, mypage_tpl, update_date) VALUES (1, '${SHOP_NAME}', '${ADMIN_MAIL}', '${ADMIN_MAIL}', '${ADMIN_MAIL}', '${ADMIN_MAIL}', '${ADMIN_MAIL}', 'default1', 'default1', 'default1', 'default1', current_timestamp);" >> ${OPTIONAL_SQL_FILE}
-
 
 case "${DBTYPE}" in
-"pgsql" ) 
+"heroku" )
     # PostgreSQL
-    echo "dropdb..."
-    sudo -u ${PGUSER} ${DROPDB} ${DBNAME}
-    echo "createdb..."
-    sudo -u ${PGUSER} ${CREATEDB} -U ${DBUSER} ${DBNAME}
     echo "create table..."
-    sudo -u ${PGUSER} ${PSQL} -U ${DBUSER} -f ${SQL_DIR}/create_table_pgsql.sql ${DBNAME}
+    ${PSQL} -h ${DBSERVER} -U ${DBUSER} -p ${DBPORT} -f ${SQL_DIR}/create_table_pgsql.sql ${DBNAME}
     echo "insert data..."
-    sudo -u ${PGUSER} ${PSQL} -U ${DBUSER} -f ${SQL_DIR}/insert_data.sql ${DBNAME}
+    ${PSQL} -h ${DBSERVER} -U ${DBUSER} -p ${DBPORT} -f ${SQL_DIR}/insert_data.sql ${DBNAME}
+    echo "create sequence table..."
+    create_sequence_tables
     echo "execute optional SQL..."
-    sudo -u ${PGUSER} ${PSQL} -U ${DBUSER} -f ${OPTIONAL_SQL_FILE} ${DBNAME}
+    get_optional_sql | ${PSQL} -h ${DBSERVER} -U ${DBUSER} -p ${DBPORT} ${DBNAME}
+    DBTYPE="pgsql"
 ;;
-"mysql" ) 
+"appveyor" | "pgsql" )
+   # PostgreSQL
+    echo "dropdb..."
+    ${PSQL} -h ${DBSERVER} -U ${DBUSER} -p ${DBPORT} -c "DROP DATABASE ${DBNAME};"
+    echo "createdb..."
+    ${PSQL} -h ${DBSERVER} -U ${DBUSER} -p ${DBPORT} -c "CREATE DATABASE ${DBNAME};"
+    echo "create table..."
+    ${PSQL} -h ${DBSERVER} -U ${DBUSER} -p ${DBPORT} -f ${SQL_DIR}/create_table_pgsql.sql ${DBNAME}
+    echo "insert data..."
+    ${PSQL} -h ${DBSERVER} -U ${DBUSER} -p ${DBPORT} -f ${SQL_DIR}/insert_data.sql ${DBNAME}
+    echo "create sequence table..."
+    create_sequence_tables
+    echo "execute optional SQL..."
+    get_optional_sql | ${PSQL} -h ${DBSERVER} -U ${DBUSER} -p ${DBPORT} ${DBNAME}
+    DBTYPE="pgsql"
+;;
+"mysql" )
+    DBPASS=`echo $DBPASS | tr -d " "`
+    if [ -n ${DBPASS} ]; then
+        PASSOPT="--password=$DBPASS"
+        CONFIGPASS=$DBPASS
+    fi
     # MySQL
     echo "dropdb..."
-    ${MYSQL} -u ${ROOTUSER} -p${ROOTPASS} -e "drop database ${DBNAME}"
+    ${MYSQL} -u ${ROOTUSER} -h ${DBSERVER} -P ${DBPORT} ${PASSOPT} -e "DROP DATABASE \`${DBNAME}\`"
     echo "createdb..."
-    ${MYSQL} -u ${ROOTUSER} -p${ROOTPASS} -e "create database ${DBNAME}"
+    ${MYSQL} -u ${ROOTUSER} -h ${DBSERVER} -P ${DBPORT} ${PASSOPT} -e "CREATE DATABASE \`${DBNAME}\` DEFAULT COLLATE=utf8_general_ci;"
     #echo "grant user..."
-    #${MYSQL} -u ${ROOTUSER} -p${ROOTPASS} -e "GRANT ALL ON ${DBNAME}.* TO '${DBUSER}'@'%' IDENTIFIED BY '${DBPASS}'"
+    #${MYSQL} -u ${ROOTUSER} -h ${DBSERVER} -P ${DBPORT} ${PASSOPT} -e "GRANT ALL ON \`${DBNAME}\`.* TO '${DBUSER}'@'%' IDENTIFIED BY '${DBPASS}'"
     echo "create table..."
-    ${MYSQL} -u ${DBUSER} -p${DBPASS} ${DBNAME} < ${SQL_DIR}/create_table_mysql.sql
+    echo "SET SESSION default_storage_engine = InnoDB; SET sql_mode = 'NO_ENGINE_SUBSTITUTION';" |
+        cat - ${SQL_DIR}/create_table_mysql.sql |
+        ${MYSQL} -h ${DBSERVER} -u ${DBUSER} -h ${DBSERVER} -P ${DBPORT} ${PASSOPT} ${DBNAME}
     echo "insert data..."
-    ${MYSQL} -u ${DBUSER} -p${DBPASS} ${DBNAME} < ${SQL_DIR}/insert_data.sql
+    echo "SET CHARACTER SET 'utf8';" |
+        cat - ${SQL_DIR}/insert_data.sql |
+        ${MYSQL} -u ${DBUSER} -h ${DBSERVER} -P ${DBPORT} ${PASSOPT} ${DBNAME}
+    echo "create sequence table..."
+    create_sequence_tables
     echo "execute optional SQL..."
-    ${MYSQL} -u ${DBUSER} -p${DBPASS} ${DBNAME} < ${OPTIONAL_SQL_FILE}
+    get_optional_sql | ${MYSQL} -u ${DBUSER} -h ${DBSERVER} -P ${DBPORT} ${PASSOPT} ${DBNAME}
 ;;
 esac
 
 #-- Setup Initial Data
-
 echo "copy images..."
 cp -rv "./html/install/save_image" "./html/upload/"
 
 echo "creating ${CONFIG_PHP}..."
-cat > "./${CONFIG_PHP}" <<EOF
-<?php
-define('ECCUBE_INSTALL', 'ON');
-define('HTTP_URL', '${HTTP_URL}');
-define('HTTPS_URL', '${HTTPS_URL}');
-define('ROOT_URLPATH', '/');
-define('DOMAIN_NAME', '${DOMAIN_NAME}');
-define('DB_TYPE', '${DBTYPE}');
-define('DB_USER', '${DBUSER}');
-define('DB_PASSWORD', '${DBPASS}');
-define('DB_SERVER', '${DBSERVER}');
-define('DB_NAME', '${DBNAME}');
-define('DB_PORT', '${DBPORT}');
-define('ADMIN_DIR', 'admin/');
-define('ADMIN_FORCE_SSL', FALSE);
-define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
-define('AUTH_MAGIC', '${AUTH_MAGIC}');
-define('PASSWORD_HASH_ALGOS', 'sha256');
-define('MAIL_BACKEND', 'mail');
-define('SMTP_HOST', '');
-define('SMTP_PORT', '');
-define('SMTP_USER', '');
-define('SMTP_PASSWORD', '');
-
-EOF
+create_config_php
 
 echo "Finished Successful!"
